0ecdf101e83908a9d6b58c86c364b1b3a8c89a24
Ítalo Vieira <italogomesv@gmail.com>
2022-02-26 01:36:33 -0300

    Add poison and HTTPoison deps

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Pokepet.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:poison, "~> 5.0"},
+      {:httpoison, "~> 1.8"},
     ]
   end
 
