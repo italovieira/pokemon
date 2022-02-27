630018a1538214d4986b6c5dc02f8f5c813948a1
Ítalo Vieira <italogomesv@gmail.com>
2022-02-27 19:43:21 -0300

    Add call to handle invalid supplied data

--- lib/pokepet_web/controllers/fallback_controller.ex
+++ lib/pokepet_web/controllers/fallback_controller.ex
@@ -13,4 +13,11 @@ defmodule PokepetWeb.FallbackController do
     |> put_view(PokepetWeb.ErrorView)
     |> render(:"404")
   end
+
+  def call(conn, {:error, %Ecto.Changeset{}}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(PokepetWeb.ErrorView)
+    |> render(:"422")
+  end
 end
